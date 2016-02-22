f40248465d2466983f8630a62bbfd5387d3e9835
Gordon Chaffee <gordon@runtime.io>
2016-02-22 03:41:18 -0800

    Implement newtmgr log commands
    * 'logs show' displays logs from a target
    * 'logs clear' will clear logs from a target

--- newtmgr/newtmgr.go
+++ newtmgr/newtmgr.go
@@ -948,6 +948,129 @@ func configCmd() *cobra.Command {
 	return statsCmd
 }
 
+func logsShowCmd(cmd *cobra.Command, args []string) {
+	cpm, err := cli.NewCpMgr()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	profile, err := cpm.GetConnProfile(ConnProfileName)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	conn, err := transport.NewConn(profile)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	runner, err := protocol.NewCmdRunner(conn)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	req, err := protocol.NewLogsShowReq()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	nmr, err := req.EncodeWriteRequest()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	if err := runner.WriteReq(nmr); err != nil {
+		nmUsage(cmd, err)
+	}
+
+	rsp, err := runner.ReadResp()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	decodedResponse, err := protocol.DecodeLogsShowResponse(rsp.Data)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+	for i := 0; i < len(decodedResponse.Logs); i++ {
+		fmt.Println(decodedResponse.Logs[i])
+	}
+}
+
+func logsClearCmd(cmd *cobra.Command, args []string) {
+	cpm, err := cli.NewCpMgr()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	profile, err := cpm.GetConnProfile(ConnProfileName)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	conn, err := transport.NewConn(profile)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	runner, err := protocol.NewCmdRunner(conn)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	req, err := protocol.NewLogsClearReq()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	nmr, err := req.EncodeWriteRequest()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	if err := runner.WriteReq(nmr); err != nil {
+		nmUsage(cmd, err)
+	}
+
+	rsp, err := runner.ReadResp()
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	decodedResponse, err := protocol.DecodeLogsClearResponse(rsp.Data)
+	if err != nil {
+		nmUsage(cmd, err)
+	}
+
+	fmt.Printf("Return Code = %d\n", decodedResponse.ReturnCode)
+}
+
+func logsCmd() *cobra.Command {
+	logsCmd := &cobra.Command{
+		Use:   "logs",
+		Short: "Handles logs on remote instance",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+
+	showCmd := &cobra.Command{
+		Use:   "show",
+		Short: "Show logs on target",
+		Run:   logsShowCmd,
+	}
+	logsCmd.AddCommand(showCmd)
+
+	clearCmd := &cobra.Command{
+		Use:   "clear",
+		Short: "Clear logs on target",
+		Run:   logsClearCmd,
+	}
+	logsCmd.AddCommand(clearCmd)
+
+	return logsCmd
+}
+
 func parseCmds() *cobra.Command {
 	nmCmd := &cobra.Command{
 		Use:   "newtmgr",
@@ -970,6 +1093,7 @@ func parseCmds() *cobra.Command {
 	nmCmd.AddCommand(taskStatsCmd())
 	nmCmd.AddCommand(mempoolStatsCmd())
 	nmCmd.AddCommand(configCmd())
+	nmCmd.AddCommand(logsCmd())
 
 	return nmCmd
 }
