db02cba25cc8d1daa8e8111a84612ba6bb6d230f
Marko Kiiskila <marko@runtime.io>
2016-04-27 17:52:21 -0700

    newt; update the version string to 0.8.0.

--- newt/newt.go
+++ newt/newt.go
@@ -29,7 +29,7 @@ import (
 	"mynewt.apache.org/newt/util"
 )
 
-var NewtVersionStr string = "Apache Newt (incubating) version: 0.8.0-b2"
+var NewtVersionStr string = "Apache Newt (incubating) version: 0.8.0"
 var NewtLogLevel log.Level
 var newtSilent bool
 var newtQuiet bool
