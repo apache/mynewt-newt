apache/mynewt-newt

commit dd34193d3c31c5d87161343f160981fb8833c23f

newt - Fix issues with bsp.yml paths.
There are two fixes here:
1. Assume relative paths are relative to the repo base dir.  Previously,
this was the package base dir.

2. Add the local repo's name to the repo name map.  This allows absolute
repo paths to refer to the local repo by name (e.g., you can use
"@apache-mynewt-core/..." when building within the core repo).

newt/project/project.go
@@ -149,8 +149,12 @@ func (proj *Project) Repos() map[string]*repo.Repo {
 }
 
 func (proj *Project) FindRepo(rname string) *repo.Repo {
-	r, _ := proj.repos[rname]
-	return r
+	if rname == repo.REPO_NAME_LOCAL {
+		return proj.LocalRepo()
+	} else {
+		r, _ := proj.repos[rname]
+		return r
+	}
 }
 
 func (proj *Project) LocalRepo() *repo.Repo {
@@ -459,7 +463,7 @@ func (proj *Project) loadConfig() error {
 		return err
 	}
 
-	proj.repos[r.Name()] = r
+	proj.repos[proj.name] = r
 	proj.localRepo = r
 	for _, ignDir := range ignoreSearchDirs {
 		r.AddIgnoreDir(ignDir)

newt/interfaces/interfaces.go
@@ -31,6 +31,7 @@ type PackageType int
 type RepoInterface interface {
 	Name() string
 	IsLocal() bool
+	Path() string
 }
 
 type VersionReqInterface interface {

newt/pkg/bsp_package.go
@@ -52,7 +52,7 @@ func (bsp *BspPackage) resolvePathSetting(
 	if val == "" {
 		return "", nil
 	}
-	path, err := proj.ResolvePath(bsp.BasePath(), val)
+	path, err := proj.ResolvePath(bsp.Repo().Path(), val)
 	if err != nil {
 		return "", util.PreNewtError(err,
 			"BSP \"%s\" specifies invalid %s setting",
@@ -84,7 +84,7 @@ func (bsp *BspPackage) resolveLinkerScriptSetting(
 
 		// Read each linker script from the list.
 		for _, val := range vals {
-			path, err := proj.ResolvePath(bsp.BasePath(), val)
+			path, err := proj.ResolvePath(bsp.Repo().Path(), val)
 			if err != nil {
 				return nil, util.PreNewtError(err,
 					"BSP \"%s\" specifies invalid %s setting",

commit f9159f5d8135bf6a5551718a33f6d7b3a23686a3

newt - Additional bug fixes for local repo check.
These fix additional bugs introduced in commit
dd34193d3c31c5d87161343f160981fb8833c23f.  Sorry, my initial commit was
sloppy.

newt/project/project.go
@@ -292,8 +292,8 @@ func (proj *Project) checkDeps(r *repo.Repo) error {
 
 func (proj *Project) UpdateRepos() error {
 	repoList := proj.Repos()
-	for rname, r := range repoList {
-		if rname == repo.REPO_NAME_LOCAL {
+	for _, r := range repoList {
+		if r.IsLocal() {
 			continue
 		}
 
@@ -327,7 +327,7 @@ func (proj *Project) Install(upgrade bool, force bool) error {
 	}
 
 	for rname, r := range proj.Repos() {
-		if rname == repo.REPO_NAME_LOCAL {
+		if r.IsLocal() {
 			continue
 		}
 		// Check the version requirements on this repository, and see

newt/repo/repo.go
@@ -294,7 +294,7 @@ func (r *Repo) Path() string {
 }
 
 func (r *Repo) IsLocal() bool {
-	return r.name == REPO_NAME_LOCAL
+	return r.local
 }
 
 func (r *Repo) VersionRequirements() []interfaces.VersionReqInterface {