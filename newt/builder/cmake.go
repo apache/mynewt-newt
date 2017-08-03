b03c6530a57060212919c895c92d036b7900be3f
Michał Narajowski <michal.narajowski@codecoup.pl>
2017-08-03 10:33:42 +0200

    use set_target_properties to set flags

--- newt/builder/cmake.go
+++ newt/builder/cmake.go
@@ -36,11 +36,17 @@ import (
 
 const CMAKELISTS_FILENAME string = "CMakeLists.txt"
 
+const (
+	COMPILER_TYPE_C       = 0
+	COMPILER_TYPE_ASM     = 1
+	COMPILER_TYPE_CPP     = 2
+)
+
 func CmakeListsPath() string {
 	return project.GetProject().BasePath + "/" + CMAKELISTS_FILENAME
 }
 
-func (b *Builder) Generate(w io.Writer, name string) error {
+func (b *Builder) Generate(w io.Writer, name string, c *toolchain.Compiler) error {
 	//b.CleanArtifacts()
 
 	// Build the packages alphabetically to ensure a consistent order.
@@ -65,17 +71,16 @@ func (b *Builder) Generate(w io.Writer, name string) error {
 		for _, s := range subEntries {
 			files = append(files, s.Filename)
 		}
-		c := subEntries[0].Compiler
 
 		builtPackages = append(builtPackages, bpkg)
 
 		fmt.Fprintf(w, "# Generating code for %s\n\n", bpkg.rpkg.Lpkg.Name())
 
-		fmt.Fprintf(w, "add_library(%s_%s OBJECT %s)\n\n",
-			name, bpkg.rpkg.Lpkg.EscapedName(),
+		fmt.Fprintf(w, "add_library(%s %s)\n\n",
+			bpkg.rpkg.Lpkg.EscapedName(),
 			strings.Join(files, " "))
 
-		CmakeCompilerInfoWrite(w, name, bpkg, c)
+		CmakeCompilerInfoWrite(w, bpkg, subEntries[0])
 
 		fmt.Printf("%s\n", bpkg.rpkg.Lpkg.BasePath())
 	}
@@ -85,35 +90,97 @@ func (b *Builder) Generate(w io.Writer, name string) error {
 	var targetObjectsBuffer bytes.Buffer
 
 	for _, bpkg := range builtPackages {
-		targetObjectsBuffer.WriteString(fmt.Sprintf("$<TARGET_OBJECTS:%s_%s> ",
-			name, bpkg.rpkg.Lpkg.EscapedName()))
+		targetObjectsBuffer.WriteString(fmt.Sprintf("%s ",
+			bpkg.rpkg.Lpkg.EscapedName()))
+
 	}
-	fmt.Fprintf(w, "add_library(%s %s)\n\n", name, targetObjectsBuffer.String())
-	fmt.Fprintf(w, ` set_target_properties(%s
-									PROPERTIES ARCHIVE_OUTPUT_DIRECTORY %s
-									LIBRARY_OUTPUT_DIRECTORY %s
-									RUNTIME_OUTPUT_DIRECTORY %s
-									)`,
+
+	fmt.Fprintln(w, "file(WRITE null.c \"\")")
+	fmt.Fprintf(w, "add_executable(%s null.c)\n\n", name)
+
+	fmt.Fprintf(w, `
+	target_link_libraries(%s
+							-Wl,--start-group %s -Wl,--end-group)`,
+		name,
+		targetObjectsBuffer.String())
+
+	fmt.Fprintln(w)
+	fmt.Fprintf(w, "set_target_properties(%s PROPERTIES COMPILE_FLAGS %s)\n\n",
+		name,
+		strings.Replace(strings.Join(append(c.GetCompilerInfo().Cflags,
+			c.GetLocalCompilerInfo().Cflags...), ","), " ", ",", -1))
+
+	fmt.Fprintf(w, `
+	set_target_properties(%s
+							PROPERTIES
+							ARCHIVE_OUTPUT_DIRECTORY %s
+							LIBRARY_OUTPUT_DIRECTORY %s
+							RUNTIME_OUTPUT_DIRECTORY %s
+							OUTPUT_DIRECTORY %s
+							LINK_FLAGS %s
+							LINKER_LANGUAGE C)`,
 		name,
 		filepath.Dir(b.AppElfPath()),
 		filepath.Dir(b.AppElfPath()),
-		filepath.Dir(b.AppElfPath()))
+		filepath.Dir(b.AppElfPath()),
+		filepath.Dir(b.AppElfPath()),
+		strings.Join(append(c.GetCompilerInfo().Lflags,
+			c.GetLocalCompilerInfo().Lflags...), ";"))
 
+	fmt.Fprintln(w)
+	for _, ld := range c.LinkerScripts {
+		fmt.Fprintf(w, `
+			set_target_properties(%s
+									PROPERTIES
+									LINK_DEPENDS %s)`,
+			name,
+			ld)
+		fmt.Fprintln(w)
+	}
 
 	return nil
 }
-func CmakeCompilerInfoWrite(w io.Writer, target_name string, bpkg *BuildPackage, c *toolchain.Compiler) {
-	fmt.Fprintf(w, "target_include_directories(%s_%s PUBLIC %s %s)\n\n",
-		target_name, bpkg.rpkg.Lpkg.EscapedName(), strings.Join(c.GetCompilerInfo().Includes, " "),
+func CmakeCompilerInfoWrite(w io.Writer, bpkg *BuildPackage, cj toolchain.CompilerJob) {
+	c := cj.Compiler
+
+	fmt.Fprintf(w, `
+	set_target_properties(%s
+							PROPERTIES
+							ARCHIVE_OUTPUT_DIRECTORY %s
+							LIBRARY_OUTPUT_DIRECTORY %s
+							RUNTIME_OUTPUT_DIRECTORY %s)`,
+		bpkg.rpkg.Lpkg.EscapedName(),
+		bpkg.rpkg.Lpkg.BasePath(),
+		bpkg.rpkg.Lpkg.BasePath(),
+		bpkg.rpkg.Lpkg.BasePath())
+	fmt.Fprintln(w)
+
+	compileFlags := []string{}
+
+	switch cj.CompilerType {
+	case COMPILER_TYPE_C:
+		compileFlags = append(compileFlags, c.GetCompilerInfo().Cflags...)
+		compileFlags = append(compileFlags, c.GetLocalCompilerInfo().Cflags...)
+	case COMPILER_TYPE_ASM:
+		compileFlags = append(compileFlags, c.GetCompilerInfo().Cflags...)
+		compileFlags = append(compileFlags, c.GetLocalCompilerInfo().Cflags...)
+		compileFlags = append(compileFlags, c.GetCompilerInfo().Aflags...)
+		compileFlags = append(compileFlags, c.GetLocalCompilerInfo().Aflags...)
+	case COMPILER_TYPE_CPP:
+		compileFlags = append(compileFlags, c.GetCompilerInfo().Cflags...)
+		compileFlags = append(compileFlags, c.GetLocalCompilerInfo().Cflags...)
+	}
+
+	fmt.Fprintf(w, `set_target_properties(%s
+													PROPERTIES
+													COMPILE_FLAGS %s)`,
+		bpkg.rpkg.Lpkg.EscapedName(),
+		strings.Replace(strings.Join(compileFlags, ","), " ", ",", -1))
+	fmt.Fprintln(w)
+	fmt.Fprintf(w, "target_include_directories(%s PUBLIC %s %s)\n\n",
+		bpkg.rpkg.Lpkg.EscapedName(),
+		strings.Join(c.GetCompilerInfo().Includes, " "),
 		strings.Join(c.GetLocalCompilerInfo().Includes, " "))
-	fmt.Fprintf(w, "set(CMAKE_C_FLAGS  \"${CMAKE_C_FLAGS_BACKUP} %s %s\")\n\n",
-		strings.Join(c.GetCompilerInfo().Cflags, " "),
-		strings.Join(c.GetLocalCompilerInfo().Cflags, " "))
-	fmt.Fprintf(w, "set(CMAKE_ASM_FLAGS  \"${CMAKE_ASM_FLAGS_BACKUP} %s %s ${CMAKE_C_FLAGS_BACKUP} %s %s\")\n\n",
-		strings.Join(c.GetCompilerInfo().Aflags, " "),
-		strings.Join(c.GetLocalCompilerInfo().Aflags, " "),
-		strings.Join(c.GetCompilerInfo().Cflags, " "),
-		strings.Join(c.GetLocalCompilerInfo().Cflags, " "))
 }
 
 func CmakeCompilerWrite(w io.Writer, c *toolchain.Compiler) {
@@ -124,7 +191,8 @@ func CmakeCompilerWrite(w io.Writer, c *toolchain.Compiler) {
 	fmt.Fprintf(w, "set(CMAKE_C_COMPILER %s)\n", "/usr/bin/"+c.GetCcPath())
 	fmt.Fprintf(w, "set(CMAKE_CXX_COMPILER %s)\n", "/usr/bin/"+c.GetCppPath())
 	fmt.Fprintf(w, "set(CMAKE_ASM_COMPILER %s)\n", "/usr/bin/"+c.GetAsPath())
-	fmt.Fprintf(w, "set(CMAKE_AR %s)\n", "/usr/bin/"+c.GetArPath())
+	/* TODO: cmake returns error on link */
+	//fmt.Fprintf(w, "set(CMAKE_AR %s)\n", "/usr/bin/"+c.GetArPath())
 	fmt.Fprintln(w)
 }
 
@@ -138,7 +206,7 @@ func CmakeHeaderWrite(w io.Writer, c *toolchain.Compiler) {
 	fmt.Fprintln(w)
 }
 
-func (t *TargetBuilder) CMakeGenerateTarget(w io.Writer) error {
+func (t *TargetBuilder) CMakeGenerateTarget(w io.Writer, c *toolchain.Compiler) error {
 	if err := t.PrepBuild(); err != nil {
 		return err
 	}
@@ -146,11 +214,13 @@ func (t *TargetBuilder) CMakeGenerateTarget(w io.Writer) error {
 	/* Build the Apps */
 	project.ResetDeps(t.AppList)
 
+	c.LinkerScripts = t.bspPkg.LinkerScripts
+
 	if err := t.bspPkg.Reload(t.AppBuilder.cfg.Features()); err != nil {
 		return err
 	}
 
-	if err := t.AppBuilder.Generate(w, t.target.ShortName()); err != nil {
+	if err := t.AppBuilder.Generate(w, t.target.ShortName(), c); err != nil {
 		return err
 	}
 
@@ -163,25 +233,24 @@ func CMakeGenerate(target *target.Target) error {
 	w := bufio.NewWriter(&b)
 	defer CmakeFileHandle.Close()
 
-
 	targetBuilder, err := NewTargetBuilder(target)
 	if err != nil {
 		return util.NewNewtError(err.Error())
 	}
 
 	c, err := targetBuilder.NewCompiler("")
-	if  err != nil {
+	if err != nil {
 		return err
 	}
 
 	CmakeHeaderWrite(w, c)
 
-	if err := targetBuilder.CMakeGenerateTarget(w); err != nil {
+	if err := targetBuilder.CMakeGenerateTarget(w, c); err != nil {
 		return util.NewNewtError(err.Error())
 	}
 
 	w.Flush()
 
 	CmakeFileHandle.Write(b.Bytes())
-	return  nil
+	return nil
 }
