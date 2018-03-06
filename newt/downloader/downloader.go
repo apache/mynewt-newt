apache/mynewt-newt

commit 0b41a1923141e6e5bceba7f5b48f9f6359af28d4

Initialize and update submodules after checkout.
Always initialize and update submodules on checkout.  This is necessary
for repos that contain submodules.  Without this change, such repos can
be left in a modified "(new commits)" state immediately after switching
branches.  If the submodules have already been updated, this does not
generate any network activity.

For example, before this commit, when I try to install version 1.1.0 of
mcuboot with `newt upgrade`,

    [ccollins@ccollins-mac:~/tmp/myproj3/repos/mcuboot]$ git status
    On branch v1.1.0
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)

            modified:   sim/mcuboot-sys/mbedtls (new commits)

    no changes added to commit (use "git add" and/or "git commit -a")

Subsequent update operations will fail due to the local modifications.

newt/downloader/downloader.go
@@ -136,6 +136,34 @@ func branchExists(repoDir string, branchName string) bool {
 	return err == nil
 }
 
+func initSubmodules(path string) error {
+	cmd := []string{
+		"submodule",
+		"init",
+	}
+
+	_, err := executeGitCommand(path, cmd, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateSubmodules(path string) error {
+	cmd := []string{
+		"submodule",
+		"update",
+	}
+
+	_, err := executeGitCommand(path, cmd, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // checkout does checkout a branch, or create a new branch from a tag name
 // if the commit supplied is a tag. sha1 based commits have no special
 // handling and result in dettached from HEAD state.
@@ -158,8 +186,22 @@ func checkout(repoDir string, commit string) error {
 			commit,
 		}
 	}
-	_, err := executeGitCommand(repoDir, cmd, true)
-	return err
+	if _, err := executeGitCommand(repoDir, cmd, true); err != nil {
+		return err
+	}
+
+	// Always initialize and update submodules on checkout.  This prevents the
+	// repo from being in a modified "(new commits)" state immediately after
+	// switching branches.  If the submodules have already been updated, this
+	// does not generate any network activity.
+	if err := initSubmodules(path); err != nil {
+		return err
+	}
+	if err := updateSubmodules(path); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // mergeBranches applies upstream changes to the local copy and must be
@@ -338,8 +380,7 @@ func (gd *GithubDownloader) UpdateRepo(path string, branchName string) error {
 	// anymore.
 	mergeBranch(path, branchName)
 
-	err = checkout(path, branchName)
-	if err != nil {
+	if err := checkout(path, branchName); err != nil {
 		return err
 	}
 

commit aa0f8e9be9db6469ea0c3e631aee3c9b9daadcb0

newt - Fix build errors.
This fixes the build error introduced in commit
0b41a1923141e6e5bceba7f5b48f9f6359af28d4.

newt/downloader/downloader.go
@@ -194,10 +194,10 @@ func checkout(repoDir string, commit string) error {
 	// repo from being in a modified "(new commits)" state immediately after
 	// switching branches.  If the submodules have already been updated, this
 	// does not generate any network activity.
-	if err := initSubmodules(path); err != nil {
+	if err := initSubmodules(repoDir); err != nil {
 		return err
 	}
-	if err := updateSubmodules(path); err != nil {
+	if err := updateSubmodules(repoDir); err != nil {
 		return err
 	}
 