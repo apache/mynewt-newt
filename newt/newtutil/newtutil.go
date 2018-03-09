28c58f6b011fb8450f57f1d7901c472b6560e4a3
Fabio Utzig <utzig@apache.org>
2018-03-09 08:37:53 -0300

    Make 1.4.0-dev distinct from 1.4.0 in repo compat check

--- newt/newtutil/newtutil.go
+++ newt/newtutil/newtutil.go
@@ -31,7 +31,7 @@ import (
 	"mynewt.apache.org/newt/yaml"
 )
 
-var NewtVersion Version = Version{1, 4, 0}
+var NewtVersion Version = Version{1, 3, 9999}
 var NewtVersionStr string = "Apache Newt version: 1.4.0-dev"
 var NewtBlinkyTag string = "master"
 var NewtNumJobs int
