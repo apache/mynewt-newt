c49337cc21d9612191d276b321e720dabfb7d4fa
Szymon Janc <szymon.janc@codecoup.pl>
2023-09-07 11:28:48 +0200

    Bump version to 1.12.0-dev

--- newt/newtutil/newtutil.go
+++ newt/newtutil/newtutil.go
@@ -30,12 +30,12 @@ import (
 	"mynewt.apache.org/newt/util"
 )
 
-var NewtVersion = Version{1, 11, 0}
-var NewtVersionStr = "1.11.0"
+var NewtVersion = Version{1, 11, 9900}
+var NewtVersionStr = "1.12.0-dev"
 var NewtGitHash = "unknown"
 var NewtDate = "unknown"
 
-var NewtBlinkyTag string = "mynewt_1_11_0_tag"
+var NewtBlinkyTag string = "master"
 var NewtNumJobs int
 var NewtForce bool
 var NewtAsk bool
