c6bf55652090e856e567749d7cfa6b3e7055f645
Szymon Janc <szymon.janc@codecoup.pl>
2024-11-13 10:40:45 +0100

    Prepare for Apache Mynewt 1.13.0 release

--- newt/newtutil/newtutil.go
+++ newt/newtutil/newtutil.go
@@ -30,12 +30,12 @@ import (
 	"mynewt.apache.org/newt/util"
 )
 
-var NewtVersion = Version{1, 12, 9900}
-var NewtVersionStr = "1.13.0-dev"
+var NewtVersion = Version{1, 13, 0}
+var NewtVersionStr = "1.13.0"
 var NewtGitHash = "unknown"
 var NewtDate = "unknown"
 
-var NewtBlinkyTag string = "master"
+var NewtBlinkyTag string = "mynewt_1_13_0_tag"
 var NewtNumJobs int
 var NewtForce bool
 var NewtAsk bool
